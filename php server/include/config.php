204c0f8ca4a5707dd5489ab52f4c6445da232934
RicardoLeitao <ricardo.l.1992@gmail.com>
2014-11-17 15:15:31 +0000

    php files

---
+++ php server/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "db.ist.utl.pt");
+define("DB_USER", "ist169632");
+define("DB_PASSWORD", "bogc5671");
+define("DB_DATABASE", "ist169632");
+?>
